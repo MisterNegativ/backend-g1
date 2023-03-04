022f095a4005370ed38f2dd4949bf5969212cc1b
MisterNegativ <120665517+MisterNegativ@users.noreply.github.com>
2023-03-04 19:21:58 +0600

    Add files via upload

--- lecture-3/cp_3/todo_app/views.py
+++ lecture-3/cp_3/todo_app/views.py
@@ -1,46 +1,142 @@
 from django.shortcuts import render
-
-from rest_framework import generics
-from .models import TodoList, Todo
-from .serializers import TodoListSerializer, TodoSerializer
-
-
-class TodoListListCreate(generics.ListCreateAPIView):
-    queryset = TodoList.objects.all()
-    serializer_class = TodoListSerializer
+from django.http import HttpResponse, JsonResponse
+from todo_app.models import TodoList, Todo
+from todo_app.serializers import TodoListSerializer, TodoSerializer
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 
-class TodoListRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = TodoList.objects.all()
-    serializer_class = TodoListSerializer
+@csrf_exempt
+def todo_lists_handler(request):
+    if request.method == 'GET':
+        todo_lists = TodoList.objects.all()
+        serializer = TodoListSerializer(todo_lists, many=True)
+        return JsonResponse(serializer.data, status=200, safe=False)
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        serializer = TodoListSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(data=serializer.data, status=200)
+        return JsonResponse(data=serializer.errors, status=400)
+    return JsonResponse({'message': 'Request is not supported'}, status=400)
 
 
-class TodoListTodosListCreate(generics.ListCreateAPIView):
-    serializer_class = TodoSerializer
-
-    def get_queryset(self):
-        todo_list_id = self.kwargs['todo_list_id']
-        return Todo.objects.filter(todo_list_id=todo_list_id)
-
-    def perform_create(self, serializer):
-        todo_list_id = self.kwargs['todo_list_id']
-        todo_list = TodoList.objects.get(id=todo_list_id)
-        serializer.save(todo_list=todo_list)
-
-
-class TodoListTodosRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
+def get_todo_list(pk):
+    try:
+        todo_list = TodoList.objects.get(id=pk)
+        return {
+            'status': 200,
+            'todo_list': todo_list
+        }
+    except TodoList.DoesNotExist as e:
+        return {
+            'status': 404,
+            'todo_list': None
+        }
 
 
-class TodoListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TodoList
-        fields = '__all__'
+@csrf_exempt
+def todo_list_handler(request, pk):
+    result = get_todo_list(pk)
+
+    if result['status'] == 404:
+        return JsonResponse({'message': 'Todo_list not found'}, status=404)
+
+    todo_list = result['todo_list']
+
+    if request.method == 'GET':
+        serializer = TodoListSerializer(todo_list)
+        return JsonResponse(serializer.data, status=200)
+    if request.method == 'PUT':
+        data = json.loads(request.body)
+        serializer = TodoListSerializer(data=data, instance=todo_list)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(data=serializer.data)
+        return JsonResponse(data=serializer.errors, status=400)
+    if request.method == 'DELETE':
+        todo_list.delete()
+        return JsonResponse({'message': 'Todo_list deleted successfully!'})
+    return JsonResponse({'message': 'Request is not supported'}, status=400)
 
 
-class TodoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Todo
-        fields = '__all__'
+@csrf_exempt
+def todo_list_todos_handler(request, pk):
+    result = get_todo_list(pk)
+
+    if result['status'] == 404:
+        return JsonResponse({'message': 'TodoList not found'}, status=404)
+
+    todo_list = result['todo_list']
+
+    if request.method == 'GET':
+        products = todo_list.todo_set.all()
+        serializer = TodoSerializer(products, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        data['todo_list_id'] = pk
+        serializer = TodoSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    return JsonResponse({'message': 'Request is not supported'}, status=400)
+
+
+def get_todo(pk):
+    try:
+        todo = Todo.objects.get(id=pk)
+        return {
+            'status': 200,
+            'todo': todo
+        }
+    except Todo.DoesNotExist as e:
+        return {
+            'status': 404,
+            'todo': None
+        }
+
+
+@csrf_exempt
+def todos_handler(request):
+    if request.method == 'GET':
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        serializer = TodoSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    return JsonResponse({'message': 'Request is not supported'}, status=400)
+
+
+@csrf_exempt
+def todo_handler(request, pk):
+    result = get_todo(pk)
+
+    if result['status'] == 404:
+        return JsonResponse({'message': 'Todo not found'}, status=404)
+
+    todo = result['todo']
+
+    if request.method == 'GET':
+        serializer = TodoSerializer(todo)
+        return JsonResponse(serializer.data)
+    if request.method == 'PUT':
+        data = json.loads(request.body)
+        serializer = TodoSerializer(instance=todo, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, safe=False)
+        return JsonResponse(serializer.errors, safe=False, status=400)
+    if request.method == 'DELETE':
+        todo.delete()
+        serializer = TodoSerializer(todo)
+        return JsonResponse(serializer.data, status=200, safe=False)
+    return JsonResponse({'message': 'Request is not supported'}, status=400)
 
